a1a7c675c2765d4d58cc0e643e15a60da5c1e327
Victor Delgado <valexdelgado@gmail.com>
2023-01-11 15:28:09 -0800

    initial commit for tracker proj

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import Unauthorized
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'asdf'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/feedback'
+# app.config['SQLALCHEMY_BINDS'] = {'testDB': 'sqlite:///test_feedback.db'}
+
+# use this DB when developing from work computer
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+
+app.debug = True
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPTS_REDIRECTS'] = False
+
+connect_db(app)
+app.app_context().push()
+
+@app.route('/')
+def home():
+    return 'sup'
