d649d9e21e3ed2c664e4f4db37fc806ad2e8c20c
Victor Delgado <valexdelgado@gmail.com>
2023-01-13 14:17:44 -0800

    added seed file for db

--- seed.py
+++ seed.py
@@ -4,11 +4,76 @@ from app import app
 connect_db(app)
 app.app_context().push()
 
-example = User(username='example', password='password', email='email.com')
+users = [
+    {
+        'username': 'HankHill',
+        'password': 'propane',
+        'email': 'hank@strickland.com'
+    },
+    {
+        'username': 'PeggyHill',
+        'password': 'boggle',
+        'email': 'peggy@boggle.com'
+    },
+    {
+        'username': 'BobbyHill',
+        'password': 'comedy',
+        'email': 'bobby@thatsmypurse.com'
+    },
+    {
+        'username': 'Boomhauer',
+        'password': 'dang',
+        'email': 'dangole@talkinbout.com'
+    },
+]
 
-reg_example = User.register_user(username=example.username, pwd=example.password)
+for user in users:
+    new = User(username=user['username'],
+                password=user['password'],
+                email=user['email'])
+    db.session.add(new)
+    db.session.commit()
 
+tests = [
+    {
+        'lot_num': 'RA123456-1',
+        'part_num': 'QM54321',
+        'test_name': '150C High Temp Storage',
+        'location': 'West lab',
+        'duration': 500,
+        'owner': 'John Redcorn',
+    },
+    {
+        'lot_num': 'RA572487-35',
+        'part_num': 'QM54321',
+        'test_name': '150C High Temp Storage',
+        'location': 'West lab',
+        'duration': 500,
+        'owner': 'John Redcorn',
+    },
+    {
+        'lot_num': 'RA123456-1',
+        'part_num': 'QM54321',
+        'test_name': '150C High Temp Storage',
+        'location': 'West lab',
+        'duration': 500,
+        'owner': 'John Redcorn',
+    },
+    {
+        'lot_num': 'RA123456-1',
+        'part_num': 'QM54321',
+        'test_name': '150C High Temp Storage',
+        'location': 'West lab',
+        'duration': 500,
+        'owner': 'John Redcorn',
+    },
+    {
+        'lot_num': 'RA123456-1',
+        'part_num': 'QM54321',
+        'test_name': '150C High Temp Storage',
+        'location': 'West lab',
+        'duration': 500,
+        'owner': 'John Redcorn',
+    },
 
-
-db.session.add(reg_example)
-db.session.commit()
+]
